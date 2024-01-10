fa6b7b5cb3bf8b6ec190c1fea7af17243756adbf
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-10 03:55:20 +0300

    örnek bir controlller ve actionundan gelen örnek bir view sayfası oluşturuldu

---
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/OrnekController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
