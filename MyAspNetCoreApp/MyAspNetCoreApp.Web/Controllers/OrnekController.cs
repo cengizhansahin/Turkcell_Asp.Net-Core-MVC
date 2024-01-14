fe82fb8f9786c394b5011dd1610b3414ea42fe4a
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-14 21:23:43 +0300

    Product kontroller ve ona denk gelen index oluşturuldu.

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/OrnekController.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/OrnekController.cs
@@ -2,7 +2,7 @@
 
 namespace MyAspNetCoreApp.Web.Controllers
 {
-    public class Product
+    public class Product2
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -11,7 +11,7 @@ namespace MyAspNetCoreApp.Web.Controllers
     {
         public IActionResult Index()
         {
-            var productList = new List<Product>()
+            var productList = new List<Product2>()
             {
                new(){Id=1,Name="Kalem"},
                new(){Id=2,Name="Defter"},
---
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Models;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository _productRepository;
+        public ProductController()
+        {
+            _productRepository = new ProductRepository();
+            if (!_productRepository.GettAll().Any())
+            {
+                _productRepository.AddProduct(new() { Id = 1, Name = "Kalem 1", Price = 100, Stock = 100 });
+                _productRepository.AddProduct(new() { Id = 2, Name = "Kalem 2", Price = 200, Stock = 200 });
+                _productRepository.AddProduct(new() { Id = 3, Name = "Kalem 3", Price = 300, Stock = 300 });
+            }
+        }
+        public IActionResult Index()
+        {
+            var products = _productRepository.GettAll();
+            return View(products);
+        }
+    }
+}
