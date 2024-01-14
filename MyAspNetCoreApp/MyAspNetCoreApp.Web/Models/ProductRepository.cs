573d429f2195da4d104d5da736bf73c821ea6224
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-14 20:57:27 +0300

    ProductRepostiyory içerisinde dört temel crud operasyonunu gerçekleştirdik

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/Models/ProductRepository.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Models/ProductRepository.cs
@@ -2,5 +2,32 @@
 {
     public class ProductRepository
     {
+        private static List<Product> _products;
+        public List<Product> GettAll() => _products;
+        public void AddProduct(Product product) => _products.Add(product);
+        public void RemoveProduct(int id)
+        {
+            var product = _products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+                throw new Exception($"Bu id({id}) ye ait ürün bulunamadı!");
+            else
+                _products.Remove(product);
+        }
+        public void UpdateProduct(Product updateProduct)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == updateProduct.Id);
+            if (product == null)
+            {
+                throw new Exception($"Bu id({updateProduct}) ye ait ürün bulunamadı!");
+            }
+            else
+            {
+                product.Name = updateProduct.Name;
+                product.Stock = updateProduct.Stock;
+                product.Price = updateProduct.Price;
+                var index = _products.FindIndex(x => x.Id == updateProduct.Id);
+                _products[index] = product;
+            }
+        }
     }
 }
