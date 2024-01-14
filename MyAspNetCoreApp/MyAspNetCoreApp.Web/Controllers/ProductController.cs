b28c700fe9b1400ac49fde46df4ad4b097c9c509
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-14 22:00:16 +0300

    Remove için buton eklendi ve silme işlemi productcontroller düzenlenerek gerçekleştirildi

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/ProductController.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/ProductController.cs
@@ -9,17 +9,16 @@ namespace MyAspNetCoreApp.Web.Controllers
         public ProductController()
         {
             _productRepository = new ProductRepository();
-            if (!_productRepository.GettAll().Any())
-            {
-                _productRepository.AddProduct(new() { Id = 1, Name = "Kalem 1", Price = 100, Stock = 100 });
-                _productRepository.AddProduct(new() { Id = 2, Name = "Kalem 2", Price = 200, Stock = 200 });
-                _productRepository.AddProduct(new() { Id = 3, Name = "Kalem 3", Price = 300, Stock = 300 });
-            }
         }
         public IActionResult Index()
         {
             var products = _productRepository.GettAll();
             return View(products);
+        }
+        public IActionResult Remove(int id)
+        {
+            _productRepository.RemoveProduct(id);
+            return RedirectToAction("Index");
         }
     }
 }
--- MyAspNetCoreApp/MyAspNetCoreApp.Web/Models/ProductRepository.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Models/ProductRepository.cs
@@ -2,7 +2,11 @@
 {
     public class ProductRepository
     {
-        private static List<Product> _products = new List<Product>();
+        private static List<Product> _products = new(){
+            new() { Id = 1, Name = "Kalem 1", Price = 100, Stock = 100 },
+            new() { Id = 2, Name = "Kalem 2", Price = 200, Stock = 200 },
+            new() { Id = 3, Name = "Kalem 3", Price = 300, Stock = 300 }
+        };
         public List<Product> GettAll() => _products;
         public void AddProduct(Product product) => _products.Add(product);
         public void RemoveProduct(int id)
