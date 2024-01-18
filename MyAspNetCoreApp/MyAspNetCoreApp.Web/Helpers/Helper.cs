1d5e6c3911b00af3cc88795aa0c4ad964eb786db
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-18 05:36:45 +0300

    Asp.Net Core DI Conteiner ile Transient yaşam döngüsü işlendi

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/Helpers/Helper.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Helpers/Helper.cs
@@ -1,9 +1,19 @@
-﻿namespace MyAspNetCoreApp.Web.Helpers
+﻿using MyAspNetCoreApp.Web.Models;
+
+namespace MyAspNetCoreApp.Web.Helpers
 {
     public class Helper : IHelper
     {
+        private readonly AppDbContext _context;
+
+        public Helper(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public string Upper(string text)
         {
+            _context.Products.ToList();
             return text.ToUpper();
         }
     }
--- MyAspNetCoreApp/MyAspNetCoreApp.Web/Program.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon")));
-builder.Services.AddScoped<IHelper, Helper>();
+builder.Services.AddTransient<IHelper, Helper>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
