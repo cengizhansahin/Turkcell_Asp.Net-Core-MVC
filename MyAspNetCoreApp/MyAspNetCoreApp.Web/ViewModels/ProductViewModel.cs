432b96d2a2b45d5e90c023359707b488f81d09df
Cengizhan <cengizhan7sahin@gmail.com>
2024-02-10 02:43:52 +0300

    regularexpression  ile price değeri decimal olduğu için düzgün çalışmadı

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -8,10 +8,12 @@ namespace MyAspNetCoreApp.Web.ViewModels
         [Required(ErrorMessage = "İsim alanı boş olamaz!")]
         [StringLength(50, ErrorMessage = "İsim alanına en fazla 50 karakter girilebilir!")]
         public string Name { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})?$", ErrorMessage = "Noktadan sonra en fazla iki basamak girebilirsiniz!")]
         [Required(ErrorMessage = "Fiyat alanı boş olamaz!")]
         [Range(1, 1000, ErrorMessage = "Fiyat alanı 1 ile 1000 arasında bir değer olmalıdır!")]
-
         public decimal? Price { get; set; }
+
         [Required(ErrorMessage = "Stok alanı boş olamaz!")]
         [Range(1, 200, ErrorMessage = "Stok alanı 1 ile 200 arasında bir değer olmalıdır!")]
         public int? Stock { get; set; }
