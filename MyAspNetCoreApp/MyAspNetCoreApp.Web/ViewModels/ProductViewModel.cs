6559d5a0761ecbcf39ba2a7ac9d0dd23996f19dd
Cengizhan <cengizhan7sahin@gmail.com>
2024-02-10 01:04:08 +0300

    Form Validation işlemlerinde kullanılacak EmailAddress attribute konusu anlatılmaktadır.

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -29,5 +29,7 @@ namespace MyAspNetCoreApp.Web.ViewModels
         [Required(ErrorMessage = "Yayınlanma süresi boş olamaz!")]
 
         public int? Expire { get; set; }
+        [EmailAddress(ErrorMessage = "Email adresini uygun formatta değil!")]
+        public string EmailAddress { get; set; }
     }
 }
