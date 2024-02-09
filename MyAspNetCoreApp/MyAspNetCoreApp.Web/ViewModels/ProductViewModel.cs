4f8b3ab1d0c78f4e3b78d535c2bb2a6dfe12e878
Cengizhan <cengizhan7sahin@gmail.com>
2024-02-09 17:23:04 +0300

    Form Validation işlemlerinde kullanılacak Required attribute konusu işlendi.

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -5,12 +5,20 @@ namespace MyAspNetCoreApp.Web.ViewModels
     public class ProductViewModel
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "İsim alanı boş olamaz!")]
         public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
+        [Required(ErrorMessage = "Fiyat alanı boş olamaz!")]
+
+        public decimal? Price { get; set; }
+        [Required(ErrorMessage = "Stok alanı boş olamaz!")]
+
+        public int? Stock { get; set; }
+        [Required(ErrorMessage = "Açıklama alanı boş olamaz!")]
+
         public string Description { get; set; }
+
         public string? Color { get; set; }
+
         public DateTime? PublishDate { get; set; }
         public bool IsPublish { get; set; }
         public int Expire { get; set; }
