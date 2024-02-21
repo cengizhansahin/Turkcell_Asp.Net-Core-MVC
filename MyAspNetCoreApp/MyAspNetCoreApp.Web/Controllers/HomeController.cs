6bb33055ad2317756204921ea96e052a5cdbf0bd
Cengizhan <cengizhan7sahin@gmail.com>
2024-02-21 15:46:14 +0300

    Partial View'lere model binding işleminin nasıl yapılacağı işlendi.

--- MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/HomeController.cs
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using MyAspNetCoreApp.Web.Helpers;
 using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
 using System.Diagnostics;
 
 namespace MyAspNetCoreApp.Web.Controllers
@@ -8,14 +10,23 @@ namespace MyAspNetCoreApp.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDbContext _appDbContext;
+        public HomeController(ILogger<HomeController> logger, AppDbContext appDbContext)
         {
             _logger = logger;
+            _appDbContext = appDbContext;
         }
 
         public IActionResult Index()
         {
+            var products = _appDbContext.Products.OrderByDescending(x => x.Id).Select(x => new ProductPartialViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                Stock = x.Stock,
+            }).ToList();
+            ViewBag.ProductListPartialViewModel = new ProductListPartialViewModel() { Products = products };
             return View();
         }
 
