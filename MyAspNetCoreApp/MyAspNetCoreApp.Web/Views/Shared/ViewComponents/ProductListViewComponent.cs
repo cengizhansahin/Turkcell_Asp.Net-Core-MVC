535380b4795f079e77813de0aca3a83b3107559b
Cengizhan <cengizhan7sahin@gmail.com>
2024-02-21 17:02:45 +0300

    ProductListViewComponent sınıfı oluşturuldu  ve sınıf içerisinde component e liste gönderildi

---
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/Views/Shared/ViewComponents/ProductListViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.Views.Shared.ViewComponents
+{
+    public class ProductListViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public ProductListViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var viewmodels = _context.Products.Select(x => new ProductListComponentViewModel()
+            {
+                Name = x.Name,
+                Description = x.Description,
+            }).ToList();
+            return View(viewmodels);
+        }
+    }
+}
