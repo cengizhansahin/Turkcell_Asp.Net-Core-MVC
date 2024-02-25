cabefccc83865aa9a5e823dd3e726159776c9483
Cengizhan <cengizhan7sahin@gmail.com>
2024-02-25 03:37:38 +0300

    Custom Tag Helper konusu örnek bir senaryo üzerinden işlendi

---
+++ MyAspNetCoreApp/MyAspNetCoreApp.Web/TagHelpers/ImageThumbnail.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyAspNetCoreApp.Web.TagHelpers
+{
+    public class ImageThumbnail : TagHelper
+    {
+        public string ImageSrc { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+            string fileName = ImageSrc.Split(".")[0];
+            string fileExtension = Path.GetExtension(ImageSrc);
+            output.Attributes.SetAttribute("src", $"{fileName}-100x100{fileExtension}");
+        }
+    }
+}
